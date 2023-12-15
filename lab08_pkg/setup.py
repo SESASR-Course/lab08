863da9e87d4008d38791890067b7e60e02b3d58f
Andrea Ostuni <andrea.ostuni@polito.it>
2023-12-15 10:59:15 +0100

    initial commit odom and amcl example

---
+++ lab08_pkg/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+import os
+from glob import glob
+
+package_name = 'lab08_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*launch.py')),
+        (os.path.join('share', package_name, 'map'), glob('map/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Andrea Ostuni',
+    maintainer_email='andrea.ostuni@polito.it',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
